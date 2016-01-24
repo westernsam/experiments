ec8bad218ce21c2e9d880ad14fd7642be2fb7ec4
westernsam <sam.owen.consultant@springer.com>
2016-01-24 16:20:06 +0000

    Make a toJson method that takes a writer

--- src/main/scala/JsonSerialization.scala
+++ src/main/scala/JsonSerialization.scala
@@ -1,30 +1,64 @@
+import java.io._
 
 object JsonSerialization {
+
   def toJson(m: Map[String, Any]): String = {
+    val w: StringWriter = new StringWriter()
+    toJson(m, w)
+    w.toString
+  }
 
-    def jsonify(any: Any): String = any match {
+  def toJson(m: Map[String, Any], outputStream: OutputStream): Unit = toJson(m, new OutputStreamWriter(outputStream))
+
+  /*
+  *
+  * Large strings can cause GC pressure - because they require contiguous char[].
+  * If you are only creating some JSON to write it somewhere else then it make sense not to store it in a string first.
+  * This version is not tail recursive: so would cause a StackOverflow for a deeply recursive structure (normally the 1000th map within map)
+  * */
+  def toJson(m: Map[String, Any], w: Writer): Unit = {
+    def writeAny(any: Any): Unit = any match {
       case map: Map[_, Any] =>
-        map.map {
-          case (key: String, value) => s""""$key":${jsonify(value)}"""
-          case t => throw new IllegalArgumentException(s"Can't jsonify maps with keys that are not Strings. Key ${t._1} is a ${t._1.getClass.getName}")
-        }.mkString("{", ",", "}")
+        w.append('{')
+        for {((key, value), index) <- map.zipWithIndex} {
+          if (!key.isInstanceOf[String])
+            throw new IllegalArgumentException(s"Can't jsonify maps with keys that are not Strings. Key $key is a ${key.getClass.getName}")
 
-      case list: Traversable[Any] =>
-        list.map(jsonify).mkString("[", ",", "]")
+          w.append(s""""$key":""")
+          writeAny(value)
+          if (index + 1 < map.size)
+            w.write(",")
+        }
+        w.append('}')
+
+      case it: Iterable[Any] =>
+        w.append('[')
+        for {(elem, index) <- it.zipWithIndex} {
+          writeAny(elem)
+          if (index + 1 < it.size)
+            w.write(",")
+        }
+        w.append(']')
 
       //arrays are not traversable
       case array: Array[Any] =>
-        array.map(jsonify).mkString("[", ",", "]")
+        w.append('[')
+        for {(elem, index) <- array.zipWithIndex} {
+          writeAny(elem)
+          if (index + 1 < array.length)
+            w.write(",")
+        }
+        w.append(']')
 
-      case str: String => s""""$str""""
-      case boolean: Boolean => s"$boolean"
-      case number: Number => s"${number.toString}"
+      case str: String => w.write(s""""$str"""")
+      case boolean: Boolean => w.write(s"$boolean")
+      case number: Number => w.write(s"${number.toString}")
+      case nil if nil == null => w.write("null")
 
-      case nil if nil == null => s"null"
       case _ => throw new IllegalArgumentException(s"Can't jsonify $any of type ${any.getClass.getName}")
     }
 
-    jsonify(m)
+    writeAny(m)
   }
 
 }
